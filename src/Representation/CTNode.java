e112e7be874eae7336a8f8bb6168e8f0eecaa859
Maxance Villame <maxance.villame@gmail.com>
2024-05-13 11:02:28 +0200

    modif

--- src/Representation/CTNode.java
+++ src/Representation/CTNode.java
@@ -3,22 +3,13 @@ package Representation;
 import java.util.HashMap;
 
 public class CTNode {
-	
-	private static HashMap<ANode, ANode[]> graphOfGame = new HashMap<>();
-	
-	public static HashMap<ANode, ANode[]> getGraphOfGame() {
-		return graphOfGame;
-	}
 
 	public CTNode() {
 		ANode start = new TNodeDecision(0, null);
 		ANode milieu = new TNodeDecision(1,null);
 		ANode fin = new TNodeDecision(2,null);
 		
-		graphOfGame.put(start, new ANode[] {milieu, fin});
-		
-		
-		
+//		start.setChoix(ANode[] {milieu,fin});
 		
 	}
 }
