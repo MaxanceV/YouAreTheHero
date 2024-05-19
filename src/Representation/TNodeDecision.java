95bbb17e3a613e037d0e6a2770e0e609933e549d
21-Ferdi-21 <155758596+21-Ferdi-21@users.noreply.github.com>
2024-05-19 14:53:49 +0200

    Update TNodeDecision.java

--- src/Representation/TNodeDecision.java
+++ src/Representation/TNodeDecision.java
@@ -1,21 +1,34 @@
 package Representation;
 
+import java.util.Scanner;
+
 public final class TNodeDecision extends ANodeInner {
 	
-	public TNodeDecision(int id, String desc) {
-		super(id, desc);
-		// TODO Auto-generated constructor stub
+	public TNodeDecision(int id, String desc, String descchoix) {
+		super(id, desc, descchoix);
 	}
 
 	@Override
 	public ANode chooseNext() {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Que faire ?");
+		for(int i=0;i< this.getChoix().length;i++) {
+			System.out.println(this.getChoix()[i].getDescriptionChoix()+" Entrez : "+i);
+		}
+		int decisionprise = scanner.nextInt();
+		this.getChoix()[decisionprise].startNode();
 		return null;
 	}
 
 	@Override
 	public void startNode() {
-		// TODO Auto-generated method stub
-		
+		// Afficher la description puis passer au choix : 
+		System.out.println(this.getDescription());
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.chooseNext();
 	}
 }
