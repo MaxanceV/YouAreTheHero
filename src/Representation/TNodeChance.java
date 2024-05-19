c875496a7a070ed47241f668a5efd14eb82c132e
21-Ferdi-21 <155758596+21-Ferdi-21@users.noreply.github.com>
2024-05-19 14:53:35 +0200

    Update TNodeChance.java

--- src/Representation/TNodeChance.java
+++ src/Representation/TNodeChance.java
@@ -1,22 +1,32 @@
 package Representation;
 
 public final class TNodeChance extends ANodeInner {
-
-	public TNodeChance(int id, String desc) {
-		super(id, desc);
-		// TODO Auto-generated constructor stub
+	
+	public TNodeChance(int id, String desc, String descchoix) {
+		super(id, desc,descchoix);
 	}
 
 	@Override
 	public ANode chooseNext() {
-		// TODO Auto-generated method stub
+		System.out.println("Que va-t-il se passer ? Tous les événements ont une égale probabilité de se produire");
+		for(int i=0;i< this.getChoix().length;i++) {
+			System.out.println(this.getChoix()[i].getDescriptionChoix());
+		}
+		// TO DO
+		//this.getChoix()[decisionprise].startNode();
 		return null;
 	}
 
 	@Override
 	public void startNode() {
-		// TODO Auto-generated method stub
-		
+		// Afficher la description puis passer au tirage aléatoire : 
+		System.out.println(this.getDescription());
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.chooseNext();
 	}
 
 }
