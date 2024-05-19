28d227615bca91c9669a35e2835441802c05c1d6
21-Ferdi-21 <155758596+21-Ferdi-21@users.noreply.github.com>
2024-05-19 14:52:31 +0200

    Update ANodeInner.java

--- src/Representation/ANodeInner.java
+++ src/Representation/ANodeInner.java
@@ -1,12 +1,21 @@
 package Representation;
 
 public abstract class ANodeInner extends ANode {
-
-	public ANodeInner(int id, String desc) {
-		super(id, desc);
-		// TODO Auto-generated constructor stub
-	}
+	// Dispose d'une liste des ID des prochains noeuds. Les enfants d'InnerNode, decision et chance iront chercher
+	// la description des choix dans le tableau des Nodes pour les proposer au joueur. 
+	private ANode[] choix;
 	
-	public abstract ANode chooseNext(); //renvoie le noeud suivant
+	public ANode[] getChoix() {
+		return choix;
+	}
 
+	public void setChoix(ANode[] choix) {
+		this.choix = choix;
+	}
+
+	public ANodeInner(int id, String desc, String descchoix) {
+		super(id, desc, descchoix);
+	}
+
+	public abstract ANode chooseNext(); //renvoie le noeud suivant
 }
