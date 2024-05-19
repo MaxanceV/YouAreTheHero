f27503389d9f7dc01d434f83ec93d4846716eb57
21-Ferdi-21 <155758596+21-Ferdi-21@users.noreply.github.com>
2024-05-19 14:54:39 +0200

    Update TNodeTerminal.java

--- src/Representation/TNodeTerminal.java
+++ src/Representation/TNodeTerminal.java
@@ -2,15 +2,19 @@ package Representation;
 
 public final class TNodeTerminal extends ANode {
 
-	public TNodeTerminal(int id, String desc) {
-		super(id, desc);
-		// TODO Auto-generated constructor stub
+	public TNodeTerminal(int id, String desc, String descchoix) {
+		super(id, desc, descchoix);
 	}
 
 	@Override
 	public void startNode() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(this.getDescription());
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Fin du jeu. Recommencer ?");
 	}
 
 }
