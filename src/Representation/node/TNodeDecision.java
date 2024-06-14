aa27ff838e9a586547a8783f2ab08130d3ca8f68
21-Ferdi-21 <155758596+21-Ferdi-21@users.noreply.github.com>
2024-06-14 16:54:46 +0200

    Update TNodeDecision.java

--- src/Representation/node/TNodeDecision.java
+++ src/Representation/node/TNodeDecision.java
@@ -11,8 +11,8 @@ import Univers.TPlayer;
 
 public final class TNodeDecision extends ANodeInner {
 	
-	public TNodeDecision(String id, String desc, String descchoix) {
-		super(id, desc, descchoix);
+	public TNodeDecision(String id, String desc, String descchoix, String image, String son) {
+		super(id, desc, descchoix, image, son);
 	}
 
 //	@Override
