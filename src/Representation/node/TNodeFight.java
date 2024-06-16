67b2ef6fea9038c4e3f973b702f68229bb17be8e
Maxance V <129884027+MaxanceV@users.noreply.github.com>
2024-06-16 17:05:38 +0200

    Update TNodeFight.java

--- src/Representation/node/TNodeFight.java
+++ src/Representation/node/TNodeFight.java
@@ -1,32 +1,42 @@
 package Representation.node;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
+import InterfaceGame.DialogNodeFight;
 import Univers.TPersonnage;
 import Univers.TPlayer;
 
 public final class TNodeFight extends ANodeInner {
 	private TPersonnage ennemi;
 
-	public TNodeFight(String id, String desc, String descchoix,TPersonnage ennemi) {
-		super(id, desc, descchoix);
+	public TNodeFight(String id, String desc, String descchoix, String image, String son, TPersonnage ennemi) {
+		super(id, desc, descchoix, image, son);
 		this.ennemi = ennemi;
 	}
 
 	@Override
 	public void setChoix(ANode[] choix) {
-		if(choix.length == 1)super.setChoix(choix);
-		else {System.out.println("Un seul choix pour les nodes de combat");}
+		if(choix.length == 2) {
+			super.setChoix(choix);
+		} else {
+			System.out.println("Deux choix pour les nodes de combat (Victoire et defaite");
+		}
 	}
 
 	@Override
 	public void launchNode(TPlayer joueur, JFrame frame) {
-		// TODO Auto-generated method stub
-		
+		SwingUtilities.invokeLater(() -> {
+            new DialogNodeFight(frame, joueur, this).setVisible(true);
+        });
 	}
 
 	@Override
 	public ENodeType getNodeType() {
 		return ENodeType.FIGHT;
+	}
+
+	public TPersonnage getEnnemi() {
+		return ennemi;
 	}
 }
